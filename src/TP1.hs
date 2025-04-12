28d0fe8297bbed0be9a176cfae3cb58f90b9909c
facu-sanabria <facu06.sanabria@gmail.com>
2025-04-12 16:36:01 -0300

    Funcion estacion realizada, con los test funcionando

--- src/TP1.hs
+++ src/TP1.hs
@@ -16,5 +16,23 @@ mesSiguiente mes
     |esMes mes = mes + 1
     
 
+multiploDe :: Number->Number->Bool
+multiploDe divisor numero = numero `mod` divisor == 0
+
+estacionMesSinCambio :: Number -> String
+estacionMesSinCambio 1 = "verano"
+estacionMesSinCambio 2 = "verano"
+estacionMesSinCambio 4 = "otonio"
+estacionMesSinCambio 5 = "otonio"
+estacionMesSinCambio 7 = "invierno"
+estacionMesSinCambio 8 = "invierno"
+estacionMesSinCambio 10 = "primavera"
+estacionMesSinCambio 11 = "primavera"
+
+estacionMesConCambio :: Number -> String
+estacionMesConCambio mes = (estacionMesSinCambio (mesAnterior mes)) ++ "/" ++  (estacionMesSinCambio (mesSiguiente mes))
+
 estacion :: Number -> String
-estacion = undefined
+estacion mes 
+    |multiploDe 3 mes = estacionMesConCambio mes
+estacion mes = estacionMesSinCambio mes
