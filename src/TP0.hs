048d364930210c49ab94c4e2453ffde095039c18
facu-sanabria <facu06.sanabria@gmail.com>
2025-04-12 16:04:08 -0300

    Funciones esMes , mesAnterior y mesSiguiente realizadas y testiadas

--- src/TP0.hs
+++ src/TP0.hs
@@ -1,6 +1,7 @@
 module TP0 where
 import PdePreludat
 import GHC.Float (truncateFloat)
+import Data.Int (Int)
 
 laRespuesta :: Number
 laRespuesta = 42
--- src/TP1.hs
+++ src/TP1.hs
@@ -2,13 +2,19 @@ module TP1 where
 import PdePreludat
 
 esMes :: Number -> Bool
-esMes = undefined
+esMes mes = mes > 0 && mes <=12
 
 mesAnterior :: Number -> Number
-mesAnterior = undefined
+mesAnterior mes 
+    |mes==1 = 12
+    |esMes mes = mes - 1
+    
 
 mesSiguiente :: Number -> Number
-mesSiguiente = undefined
+mesSiguiente mes
+    |mes==12 = 1
+    |esMes mes = mes + 1
+    
 
 estacion :: Number -> String
 estacion = undefined
